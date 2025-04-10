bc779a21f9c1d128336fef59903c756f575e910a
Mohammed Aman <zbat37@gmail.com>
2025-04-10 15:25:05 +0530

    Cleaning up the  cord Removing cli

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from core.backup_manager import GameBackupCore
-from ui.cli_interface import BackupUI
 from ui.gui_interface import BackupGUI
-import sys
 
 def main():
     core = GameBackupCore()
     
-   
-    if len(sys.argv) > 1 and sys.argv[1] == "--cli":
-        cli = BackupUI(core)
-        cli.show_main_menu()
-    else:
-        gui = BackupGUI(core)
-        gui.run()
+    # Launch the GUI directly without checking for CLI argument
+    gui = BackupGUI(core)
+    gui.run()
 
 if __name__ == "__main__":
     main()
